d5dc0d7345e5077ac46c3e930e7f6c6dd8a5f4c3
Helgi Möller <helgi@azazo.com>
2015-05-20 15:33:44 +0000

    Adding support for task warrior within the i3 status bar

--- i3-utils/i3bar.sh
+++ i3-utils/i3bar.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-i3status --config ~/.i3status.conf | while :
+i3status --config /home/helgi/.i3/i3status.conf | while :
 do
 	read line
 	LG=$(setxkbmap -query | awk '/layout/{print $2}')
-	echo "KB $LG | $line" || exit 1
+	TASK=$(task ls | sed -n '4s/[[:blank:]]\+/ /pg')
+
+	echo "TODO: $TASK | KB $LG | $line" || exit 1
 done
