1895f1410c4c8750cf04c77bf09d1115bba9fbe0
Helgi Moller <helgi@helgimoller.a18.lan>
2016-10-11 15:33:23 +0000

    ZSH tmux and i3 changes for fedora
    blablalbal

--- i3-utils/i3bar.sh
+++ i3-utils/i3bar.sh
@@ -4,7 +4,5 @@ i3status --config /home/helgi/.i3/i3status.conf | while :
 do
 	read line
 	LG=$(setxkbmap -query | awk '/layout/{print $2}')
-	TASK=$(task ls | sed -n '4s/[[:blank:]]\+/ /pg')
-
-	echo "TODO: $TASK | KB $LG | $line" || exit 1
+	echo "KB $LG | $line" || exit 1
 done
